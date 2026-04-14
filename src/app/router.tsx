9f3b4aa107dc84411fb488c25847cd9d2d486405
Jayvee <jayvee.github0703@gmail.com>
2026-04-14 16:58:07 +0800

    Added aside and nav bars

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import LoginForm from "../feature/auth/components/LoginForm";
-import ChatForm from "../feature/chat/components/ChatForm";
+import ChatRoom from "../feature/chat/components/ChatRoom";
 
 export default function RouterPaths() {
   return (
     <Routes>
       <Route path="/" element={<LoginForm />} />
-      <Route path="/chat-room" element={<ChatForm />} />
+      <Route path="/chat-room" element={<ChatRoom />} />
     </Routes>
   );
 }
---
+++ src/feature/chat/components/Aside.tsx
@@ -0,0 +1,28 @@
+import { Home, LogOut } from "iconoir-react";
+import { useState } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
+
+export default function Aside() {
+  const location = useLocation();
+  return (
+    <aside className="bg-blue-50 flex flex-col items-center fixed top-13 left-0 bottom-0 overflow-hidden transition-all translate-x-[-80px] md:translate-x-0 w-20 border-0 shadow-r py-1 pt-7">
+      <ul className="flex flex-col w-full items-center grow">
+        <li className=" w-full ">
+          <NavLink
+            to="/chat-room"
+            className={`${location.pathname === "chat-room" ? "" : ""} w-full flex justify-center py-3 relative`}
+          >
+            <p>
+              <Home />
+            </p>
+          </NavLink>
+        </li>
+      </ul>
+      <div className="">
+        <button className="bg-blue-600 text-white font-bold py-2 px-5 rounded-sm cursor-pointer">
+            <LogOut  />
+        </button>
+      </div>
+    </aside>
+  );
+}
